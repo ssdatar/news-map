604b36b33694ba2d3f2720f5a1a448eddaa098d4
Saurabh Datar <sdatar2@stanford.edu>
2022-04-18 11:56:39 -0400

    got map to show on page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Map from 'react-map-gl';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+// import Button from 'react-bootstrap/Button';
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid>
+      <Row>
+        <Col xs={12} md={8} lg={9}>
+          <Map
+            initialViewState={{
+              longitude: -105.358887,
+              latitude: 39.113014,
+              zoom: 6.5
+            }}
+            style={{width: '100%', height: 450}}
+            mapStyle="mapbox://styles/mapbox/light-v9"
+            mapboxAccessToken="pk.eyJ1IjoiZGF0YXJrYWxsb28iLCJhIjoiY2toOXI3aW5kMDRlZTJ4cWt0MW5kaHg4eCJ9.V4NfOecIoFaErvFv_lfKLg"
+          />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
