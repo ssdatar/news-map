29fc591db54c7e293e70f7a27deaa88f132931aa
Saurabh Datar <sdatar2@stanford.edu>
2022-04-22 18:57:14 -0400

    got a nice popup working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {
   useState, useEffect, useMemo, useCallback,
 } from 'react';
 
-import Map from 'react-map-gl';
+import Map, { Source, Layer, Popup } from 'react-map-gl';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -13,77 +13,94 @@ import groupBy from './utils';
 
 import './App.scss';
 
-function App() {
 
+function App() {
   const [allData, setAllData] = useState(null);
   const [lookup, setLookup] = useState(null);
+  const [shapeFile, setShapeFile] = useState(null);
   const [hoverInfo, setHoverInfo] = useState(null);
-  const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3OCcVgY7Sy8GBRUlrsLWJkfJnEtT5L7IqxNNRon1_Pw3keeVbNfs1h3QUFcFd9jz9cIfoIXg0MTn1/pub?gid=1853710081&single=true&output=csv';
+
+  const getData = () => {
+    const mainSheet = axios({
+        url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3OCcVgY7Sy8GBRUlrsLWJkfJnEtT5L7IqxNNRon1_Pw3keeVbNfs1h3QUFcFd9jz9cIfoIXg0MTn1/pub?gid=1853710081&single=true&output=csv', 
+        method: 'GET',
+        responseType: 'text',
+      });
+
+    const nonTraditional = axios({
+        url: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3OCcVgY7Sy8GBRUlrsLWJkfJnEtT5L7IqxNNRon1_Pw3keeVbNfs1h3QUFcFd9jz9cIfoIXg0MTn1/pub?gid=1853710081&single=true&output=csv', 
+        method: 'GET',
+        responseType: 'text',
+      });
+
+    const geoJson = axios.get('colorado.json');
+
+    axios.all([mainSheet, nonTraditional, geoJson])
+    .then(axios.spread((...responses) => {
+      setAllData(csvParse(responses[0].data));
+      setLookup(groupBy(responses[0].data));
+      setShapeFile(responses[2].data);
+    }))
+    .catch(errors => {
+      console.log(errors);
+    });
+  };
 
   useEffect(() => {
-    axios({
-      url: url, 
-      method: 'GET',
-      responseType: 'text',
-    })
-    .then((res) => {
-      const sheet = csvParse(res.data);
-      console.log(sheet);
-      setAllData(sheet);
-      setLookup(groupBy(sheet));
-    })
-    .catch((err) => {
-      console.log('Could not load data', err);
-    });
+    getData();
   }, []);
 
   
   const onHover = useCallback(event => {
     const {
       features,
-      point: {x, y}
+      point: {x, y},
+      // longitude: event.lngLat.lng,
+      // latitude: event.lngLat.lat,
     } = event;
     
     const hoveredFeature = features && features[0];
-    
-    const selectedFeature = allData.filter(d => d.COUNTY.toLowerCase() === hoveredFeature.properties.NAME.toLowerCase());
-    console.log(selectedFeature);
+    hoveredFeature.longitude = event.lngLat.lng;
+    hoveredFeature.latitude = event.lngLat.lat;
+    // const selectedFeature = allData.filter(d => d.COUNTY.toLowerCase() === hoveredFeature.properties.NAME.toLowerCase());
+    console.log(hoveredFeature && {feature: hoveredFeature, x, y});
 
     setHoverInfo(hoveredFeature && {feature: hoveredFeature, x, y});
   }, []);
 
-  const data = useMemo(() => {
-    return allData;
-  }, [ allData ]);
+  // const data = useMemo(() => {
+  //   return allData;
+  // }, [ allData ]);
 
-  // const fillColor = {
-  //   id: 'colorado',
-  //   type: 'fill',
-  //   paint: '#e5e5e5',
-  //   {
-  //     'fill-color': {
-  //       property: 'percentile',
-  //       stops: [
-  //         [0, '#3288bd'],
-  //         [1, '#66c2a5'],
-  //         [2, '#abdda4'],
-  //         [3, '#e6f598'],
-  //         [4, '#ffffbf'],
-  //         [5, '#fee08b'],
-  //         [6, '#fdae61'],
-  //         [7, '#f46d43'],
-  //         [8, '#d53e4f']
-  //       ]
-  //     },
-  //     'fill-opacity': 0.8
-  //   }
-  // };
+  const fillColor = {
+    id: 'colorado',
+    type: 'fill',
+    paint: {
+      'fill-color': '#545454',
+      'fill-outline-color': '#e5e5e5',
+      // {
+      //   property: 'percentile',
+      //   stops: [
+      //     [0, '#3288bd'],
+      //     [1, '#66c2a5'],
+      //     [2, '#abdda4'],
+      //     [3, '#e6f598'],
+      //     [4, '#ffffbf'],
+      //     [5, '#fee08b'],
+      //     [6, '#fdae61'],
+      //     [7, '#f46d43'],
+      //     [8, '#d53e4f']
+      //   ]
+      // },
+      'fill-opacity': 0.8
+    }
+  };
 
 
   return (
     <Container fluid>
       <Row>
-        <Col xs={12} md={8} lg={9}>
+        <Col xs={12} md={8} lg={8}>
           <Map
             initialViewState={{
               longitude: -105.358887,
@@ -91,11 +108,35 @@ function App() {
               zoom: 6
             }}
             style={{width: '100%', height: 450}}
-            mapStyle="mapbox://styles/datarkalloo/cl25brdxk001e14lpwvnxpjr7"
+            mapStyle="mapbox://styles/mapbox/light-v10"
+            // mapStyle="mapbox://styles/datarkalloo/cl25brdxk001e14lpwvnxpjr7"
             mapboxAccessToken="pk.eyJ1IjoiZGF0YXJrYWxsb28iLCJhIjoiY2toOXI3aW5kMDRlZTJ4cWt0MW5kaHg4eCJ9.V4NfOecIoFaErvFv_lfKLg"
             interactiveLayerIds={['colorado']}
             onMouseMove={onHover}
-          />
+            onMouseLeave={() => { setHoverInfo(null) }}>
+
+            <Source type="geojson" data={shapeFile}>
+              <Layer {...fillColor} />
+            </Source>
+
+            {hoverInfo && (
+              <Popup
+                longitude={hoverInfo.feature.longitude}
+                latitude={hoverInfo.feature.latitude}
+                offset={[0, -10]}
+                closeButton={ false }
+                className="county-info"
+              >
+                { hoverInfo.feature.properties.NAME }
+              </Popup>
+            )}
+
+            {/*{hoverInfo && (
+              <div className="tooltip" style={{left: hoverInfo.x, top: hoverInfo.y}}>
+                <div>County: {hoverInfo.feature.properties.NAME}</div>               
+              </div>
+            )}*/}
+          </Map>
         </Col>
       </Row>
     </Container>
