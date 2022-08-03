a6a0efe8b7c4ae43d9f2153ec935a854c99d0a45
SDatar <SDatar@crc-dot1x-nat-10-239-97-56.bu.edu>
2022-08-03 14:48:43 -0400

    fresh demographics

--- src/components/Race.js
+++ src/components/Race.js
@@ -4,16 +4,17 @@ function Race(props) {
   const { feature } = props;
   const { properties } = feature;
   const raceLabels = ['White', 'Black', 'Hispanic', 'Asian', 'Native American', 'Other race', 'Multiracial']
-  const raceKeys = ['white', 'black', 'hispanic', 'asianp', 'nat_am', 'other', 'multiracial'];
+  const raceKeys = ['white_pct', 'black_pct', 'hispanic_pct', 'asian_pi_pct', 'nat_am_pct', 'other_pct', 'multiracial_pct'];
 
   const raceData = raceKeys.map((k, i) => ({
     race: raceLabels[i],
-    num: Number.isNaN(+properties[raceKeys[i]]) ? 0 : +properties[raceKeys[i]],
-    pct: parseFloat((100 * (properties[raceKeys[i]] / properties.total_pop)).toFixed(1))
+    pct: Number.isNaN(+properties[raceKeys[i]]) ? 0 : (100 * properties[raceKeys[i]]).toFixed(1),
+    // pct: parseFloat((100 * (properties[raceKeys[i]] / properties.total_pop)).toFixed(1))
   }))
   .sort((a, b) => b.pct - a.pct);
 
-  // console.log(raceData);
+  console.log(properties);
+  console.log(raceData);
 
   return (
     <Table  className='source__table' striped hover responsive>    
